f7c01b77f441e434b3b557193ecd66ae3f6a518a
Hirusha Pathum <your-email@gmail.com>
2026-05-17 17:22:51 +0530

    initial project structure

---
+++ src/Movie.java
@@ -0,0 +1,15 @@
+public class Movie {
+    private int id;
+    private String title;
+    private String genre;
+
+    public Movie(int id, String title, String genre) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public int getId() { return id; }
+    public String getTitle() { return title; }
+    public String getGenre() { return genre; }
+}
